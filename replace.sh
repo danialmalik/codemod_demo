e6a96fe9596a916a70f3ab7ec30252a10065651f
danialmalik <danialmalik321@gmail.com>
2019-07-08 18:24:36 +0500

    example to change a function

--- replace.sh
+++ replace.sh
@@ -5,3 +5,17 @@ replacement=`cat output`
 codemod -m -d ./ --extensions py \
     "$source" \
     "$replacement"
+
+
+# To replace a function
+source="def to_be_replaced\(param1, param2\):\s*variable1 = 'asd'\s*print\('data: {}'\.format\(variable1\)\)\s*return variable1"
+replaced_data="def replaced(replaced_param):
+    data1 = 'acs'
+    number1 = 123
+    print('data: {}, {}'.format(data1, number1))
+    return (data1, number1)
+"
+
+codemod -m -d ./ --extensions py \
+    $source \
+    $replaced_data
---
+++ test.py
@@ -0,0 +1,4 @@
+def to_be_replaced(param1, param2):
+    variable1 = 'asd'
+    print('data: {}'.format(variable1))
+    return variable1
